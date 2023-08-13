fef676514ae2292e9dca57a239c5a4a5baf75c3a
Grant Sivley <grant.sivley@gmail.com>
2023-08-12 20:08:33 -0500

    stock data validation

--- stock.py
+++ stock.py
@@ -8,8 +8,8 @@ class Stock:
             percet_allocation (float): Percent allocation as decribed by a number from 0 - 1.00. This is the desired percent allocation to buy with the remaining money
         """
         self.ticker = ticker.upper()
-        self.current_cost = current_cost
-        self.percent_allocation = percent_allocation
+        self.current_cost = float(current_cost)
+        self.percent_allocation = float(percent_allocation)
         self.purchase_number = 0
 
     def __str__(self) -> str:
